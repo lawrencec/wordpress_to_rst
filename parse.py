22d9e9757dfff3a23217a73392130bd9afe5fe2f
lawrencec <lawrence@nodetraveller.com>
2010-04-12 22:04:50 +0000

    First commit. Moved into own repo

---
+++ parse.py
@@ -0,0 +1,137 @@
+#from xml.etree.ElementPath
+import datetime
+import subprocess
+import lxml.etree as etree
+import json
+
+from optparse import OptionParser
+
+
+class WP_Importer(object):
+    '''Wordpress Importer'''
+
+    def __init__(self, filename, do_convert):
+        self.filename = filename
+        self.do_convert = do_convert
+
+    def convert_To_Rst(self, html):
+        '''Converts html to Rst'''
+        p = subprocess.Popen(['pandoc', '--from=html', '--to=rst'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        (stdout, stderr) = p.communicate(html)
+#        if stderr != "":
+#            print 'ERROR CONVERTING THE FOLLOWING TO RST :'
+#            print 'ATTEMPTING TO CONVERT : %s' % html[0:80]
+#            print 'ERROR: %s' % stderr
+        return stdout
+
+    def parse(self):
+        data = etree.parse(self.filename)
+        root = data.find("channel")
+        blog = {
+            'title': root.findtext('title'),
+            'description': root.findtext('description'),
+            'link': root.findtext('link'),
+        }
+
+        # Categories
+        cats = root.findall('{http://wordpress.org/export/1.0/}category')
+        categories = []
+        for c in cats:
+            categories.append({
+                'name': c.findtext('{http://wordpress.org/export/1.0/}cat_name'),
+                'nice_name': c.findtext('{http://wordpress.org/export/1.0/}category_nicename'),
+                'parent': c.findtext('{http://wordpress.org/export/1.0/}parent')
+            })
+        # Tags
+        tags = []
+        tagElements = root.findall('{http://wordpress.org/export/1.0/}tag')
+        for t in tagElements:
+            tags.append({
+                'name': t.findtext('{http://wordpress.org/export/1.0/}tag_name'),
+                'tag_slug': t.findtext('{http://wordpress.org/export/1.0/}tag_slug')
+            })
+        # posts
+        posts = []
+        for post in data.findall("channel/item"):
+            p = {
+                'id': post.findtext('{http://wordpress.org/export/1.0/}post_id'),
+                'title': post.findtext('title'),
+                'name': post.findtext('{http://wordpress.org/export/1.0/}post_name'),
+                'link': post.findtext('link'),
+                'creator': post.findtext('{http://purl.org/dc/elements/1.1/}creator'),
+                'categories': dict([(c.findtext("."), "") for c in post.findall("category[@domain='category']")]).keys(),
+                'tags': dict([(c.findtext("."), "") for c in post.findall("category[@domain='tag']")]).keys(),
+                'description': post.findtext('description'),
+                'content': post.findtext('{http://purl.org/rss/1.0/modules/content/}encoded') if not self.do_convert else self.convert_To_Rst(post.findtext('{http://purl.org/rss/1.0/modules/content/}encoded')),
+                'post_date': datetime.datetime.strptime(post.findtext("{http://wordpress.org/export/1.0/}post_date_gmt"),"%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%SZ"),
+                'status': post.findtext('{http://wordpress.org/export/1.0/}status'),
+                'comments': []
+            }
+            comments = []
+            # post comments
+            post_comments = post.findall('{http://wordpress.org/export/1.0/}comment')
+
+            if (len(post_comments)):
+                for c in post_comments:
+                    cmt = {
+                        'author': c.findtext('{http://wordpress.org/export/1.0/}comment_author'),
+                        'author_email': c.findtext('{http://wordpress.org/export/1.0/}comment_author_email'),
+                        'author_url': c.findtext('{http://wordpress.org/export/1.0/}comment_author_url'),
+                        'author_ip': c.findtext('{http://wordpress.org/export/1.0/}comment_author_IP'),
+                        'post_date': datetime.datetime.strptime(c.findtext("{http://wordpress.org/export/1.0/}comment_date_gmt"),"%Y-%m-%d %H:%M:%S").strftime("%Y/%m/%d %H:%M:%SZ"),
+                        'content': c.findtext('{http://wordpress.org/export/1.0/}comment_content') if not self.do_convert else self.convert_To_Rst(c.findtext('{http://wordpress.org/export/1.0/}comment_content'))
+                    }
+                    comments.append(cmt)
+                p['comments'] = comments
+            posts.append(p)
+
+        blog['categories'] = categories
+        blog['tags'] = tags
+        blog['posts'] = posts
+
+        return blog
+
+
+def save_to_json(o, filename, append=False):
+    """Saves python object to json, optionally appending to specified filename"""
+    writemode = 'w' if not append else 'a'
+    with open(filename, writemode) as f:
+        json.dump(o, sort_keys=True, indent=4, fp=f)
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.add_option("-f", '--file', dest='filename', help='Wordpress export xml file', metavar="FILE")
+    parser.add_option("-o", '--outfile', dest='outfilename', help='Filename to save output to (JSON)', metavar="OUTFILE")
+    parser.add_option("-c", '--convert', dest='convert', help='perform conversion to RestructedText', action="store_true", default=False)
+    parser.add_option("-s", '--split-output', dest='split', help='Split data into separate files eg categories.json, tags.json, posts.json', action="store_true", default=False)
+    parser.add_option("-d", '--dry-run', dest='dryrun', help='Performs a dry run, will not save any output', action="store_true", default=False)
+
+    (options, args) = parser.parse_args()
+
+    if options.filename != None:
+        print "Parsing Wordpress export xml file : %s " % options.filename
+        print "--"
+        i = WP_Importer(options.filename, options.convert)
+        blog = i.parse()
+        print 'Number of tags found: %s ' % str(len(blog['tags']))
+        print 'Number of categories found: %s ' % str(len(blog['categories']))
+        print 'Number of posts found: %s ' % str(len(blog['posts']))
+        print "--"
+        if options.outfilename:
+            if options.split:
+                categories = blog['categories']
+                tags = blog['tags']
+                posts = blog['posts']
+                del blog['categories']
+                del blog['tags']
+                del blog['posts']
+                save_to_json(blog, options.outfilename.replace('.json', '-blog.json'), False)
+                save_to_json(categories, options.outfilename.replace('.json', '-categories.json'), False)
+                save_to_json(tags, options.outfilename.replace('.json', '-tags.json'), False)
+                save_to_json(posts, options.outfilename.replace('.json', '-posts.json'), False)
+            else:
+                save_to_json(blog, options.outfilename, False)
+    else:
+        parser.print_help()
+        exit()
---
+++ readme.md
@@ -0,0 +1,28 @@
+# Wordpress to Rst convertor
+
+Converts Wordpress blog posts in wordpress xml format to Restructured Text.
+
+## Help
+Run on commandline. Use -h option to view command line options  
+
+	-h, --help            show this help message and exit  
+	-f FILE, --file=FILE  Wordpress export xml file  
+	-o OUTFILE, --outfile=OUTFILE Filename to save output to (JSON)  
+	-c, --convert         perform conversion to RestructedText  
+	-s, --split-output    Split data into separate files eg -categories.json, -tags.json, -posts.json  
+	-d, --dry-run         Performs a dry run, will not save any output  
+
+
+## Examples:
+
+The following example will convert a wordpress xml file to json and save it in json format in a file called myblog.json
+
+	python parse.py -f myblog.xml -o myblog.json
+	
+This will convert to json and save split out the categories, tags and posts into separate files.
+
+	python parse.py -f myblog.xml -o myblog.json -s
+	
+The following will split and convert to json with any html fields converted to Rst format.
+
+	python parse.py -f myblog.xml -o myblog.json -c	-s
